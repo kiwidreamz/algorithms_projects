96e85ed1402dcf1e9a0a2cc3cac2385a5c38f096
Stephane Popov <73848169+kiwidreamz@users.noreply.github.com>
2022-04-16 18:36:09 +0200

    Add files via upload

---
+++ week 1 - hello/HelloGoodbye.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.print("Hello " + args[0] + " and " + args[1] + "\n");
+        System.out.print("Goodbye " + args[1] + " and " + args[0] + "\n");
+    }
+}
